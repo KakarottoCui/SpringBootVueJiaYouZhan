0acbda711ecf066137efa07df5ccb36b2a4da4af
cuifengbo <471363336@qq.com>
2024-09-13 10:47:06 +0800

    1

---
+++ src/views/Notice.vue
@@ -0,0 +1,313 @@
+<template>
+  <div>
+    <div style="margin: 10px 0">
+      <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="name"></el-input>
+      <!--      <el-input style="width: 200px" placeholder="请输入" suffix-icon="el-icon-message" class="ml-5" v-model="email"></el-input>-->
+      <!--      <el-input style="width: 200px" placeholder="请输入" suffix-icon="el-icon-position" class="ml-5" v-model="address"></el-input>-->
+      <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+      <el-button type="warning" @click="reset">重置</el-button>
+    </div>
+
+    <div style="margin: 10px 0">
+      <el-button type="primary" @click="handleAdd">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+      <el-popconfirm
+          class="ml-5"
+          confirm-button-text='确定'
+          cancel-button-text='我再想想'
+          icon="el-icon-info"
+          icon-color="red"
+          title="您确定批量删除这些数据吗？"
+          @confirm="delBatch"
+      >
+        <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+      </el-popconfirm>
+      <!-- <el-upload action="http://localhost:9999/notice/import" :show-file-list="false" accept="xlsx" :on-success="handleExcelImportSuccess" style="display: inline-block">
+        <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>
+      </el-upload>
+      <el-button type="primary" @click="exp" class="ml-5">导出 <i class="el-icon-top"></i></el-button> -->
+    </div>
+
+    <el-table :data="tableData" border stripe :header-cell-class-name="'headerBg'"  @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column prop="id" label="ID" width="80" sortable></el-table-column>
+      <el-table-column prop="name" label="标题"></el-table-column>
+      <el-table-column prop="content" label="内容">
+        <template v-slot="scope">
+          <el-button type="primary" @click="view(scope.row.content)">查看内容</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column prop="time" label="发布时间"></el-table-column>
+      <el-table-column label="图片"><template slot-scope="scope"><el-image style="width: 100px; height: 100px" :src="scope.row.img" :preview-src-list="[scope.row.img]"></el-image></template></el-table-column>
+
+      <el-table-column label="操作"  width="300" align="center">
+        <template slot-scope="scope">
+          <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
+          <el-button type="success" @click="handleSaveToWord(scope.row)">导出 <i class="el-icon-edit"></i></el-button>
+          <el-popconfirm
+              class="ml-5"
+              confirm-button-text='确定'
+              cancel-button-text='我再想想'
+              icon="el-icon-info"
+              icon-color="red"
+              title="您确定删除吗？"
+              @confirm="del(scope.row.id)"
+          >
+            <el-button type="danger" slot="reference">删除 <i class="el-icon-remove-outline"></i></el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="padding: 10px 0">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageNum"
+          :page-sizes="[2, 5, 10, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
+    </div>
+
+    <el-dialog title="信息" :visible.sync="dialogFormVisible" width="50%" :close-on-click-modal="false">
+      <el-form label-width="100px" size="small" style="width: 90%">
+        <el-form-item label="标题">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-upload
+              class="avatar-uploader"
+              action="http://localhost:9999/file/upload"
+              ref="img"
+              :show-file-list="false"
+              :on-success="handleImgUploadSuccess">
+            <img v-if="form.img" :src="form.img" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="内容">
+          <div id="richText"></div>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog title="内容" :visible.sync="dialogFormVisible1" width="50%" :close-on-click-modal="false">
+      <el-card>
+        <div v-html="content"></div>
+      </el-card>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import E from "wangeditor"
+import {saveAs} from 'file-saver';
+import htmlDocx from 'html-docx-js/dist/html-docx';
+let editor;
+export default {
+  name: "Notice",
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 10,
+      name: "",
+      form: {},
+      dialogFormVisible: false,
+      dialogFormVisible1: false,
+      content: '',
+      tempFileName: '111',
+      multipleSelection: [],
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods: {
+    handleSaveToWord(row) {
+      // this.content 富文本内容
+      // this.tempFileName 导出的文件名
+      var converted = htmlDocx.asBlob(row.content);
+      saveAs(converted, this.tempFileName);
+    },
+    view(content) {
+      this.content = content
+      this.dialogFormVisible1 = true
+    },
+    load() {
+      this.request.get("/notice/page", {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          name: this.name,
+          type: 2
+        }
+      }).then(res => {
+        this.tableData = res.data.records
+        this.total = res.data.total
+      })
+    },
+    save() {
+      const content = editor.txt.html()
+      console.log(content)
+      // 注意：这个地方需要手动赋值
+      this.form.content = content
+      this.form.type = 2
+      this.request.post("/notice", this.form).then(res => {
+        if (res.code === '200') {
+          this.$message.success("保存成功")
+          this.dialogFormVisible = false
+          this.load()
+        } else {
+          this.$message.error("保存失败")
+        }
+      })
+    },
+    handleAdd() {
+      this.dialogFormVisible = true
+      this.form = {img: ''}
+      this.$nextTick(() => {
+        if(!editor) {
+          editor = new E("#richText")
+          editor.config.uploadImgServer = 'http://localhost:9999/file/uploadImg'
+          editor.config.uploadFileName = 'file'
+          editor.create()
+        }
+        editor.txt.html('')  // 清除内容
+
+        if(this.$refs.img) {
+          this.$refs.img.clearFiles();
+        }
+        if(this.$refs.file) {
+          this.$refs.file.clearFiles();
+        }
+      })
+    },
+// {
+//   "errno": 0,
+//     "data": [
+    //   {
+    //     url: "图片地址"
+    //   }
+//   ]
+// }
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row))
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        if(!editor) {
+          editor = new E("#richText")
+          editor.config.uploadImgServer = 'http://localhost:9999/file/uploadImg'
+          editor.config.uploadFileName = 'file'
+          editor.create()
+        }
+        editor.txt.html(this.form.content)
+        if(this.$refs.img) {
+          this.$refs.img.clearFiles();
+        }
+        if(this.$refs.file) {
+          this.$refs.file.clearFiles();
+        }
+      })
+    },
+    del(id) {
+      this.request.delete("/notice/" + id).then(res => {
+        if (res.code === '200') {
+          this.$message.success("删除成功")
+          this.load()
+        } else {
+          this.$message.error("删除失败")
+        }
+      })
+    },
+    handleSelectionChange(val) {
+      console.log(val)
+      this.multipleSelection = val
+    },
+    delBatch() {
+      if (!this.multipleSelection.length) {
+        this.$message.error("请选择需要删除的数据")
+        return
+      }
+      let ids = this.multipleSelection.map(v => v.id)  // [{}, {}, {}] => [1,2,3]
+      this.request.post("/notice/del/batch", ids).then(res => {
+        if (res.code === '200') {
+          this.$message.success("批量删除成功")
+          this.load()
+        } else {
+          this.$message.error("批量删除失败")
+        }
+      })
+    },
+    reset() {
+      this.name = ""
+      this.load()
+    },
+    handleSizeChange(pageSize) {
+      console.log(pageSize)
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum) {
+      console.log(pageNum)
+      this.pageNum = pageNum
+      this.load()
+    },
+    handleFileUploadSuccess(res) {
+      this.form.file = res
+    },
+    handleImgUploadSuccess(res) {
+      this.form.img = res
+    },
+    download(url) {
+      window.open(url)
+    },
+    exp() {
+      window.open("http://localhost:9999/notice/export")
+    },
+    handleExcelImportSuccess() {
+      this.$message.success("导入成功")
+      this.load()
+    }
+  }
+}
+</script>
+
+
+<style>
+.headerBg {
+  background: #eee!important;
+}
+
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
